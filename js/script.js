d81e77ef6400db393f47eca69ea63cb3c4be503d
Ash! <113056162+ashley-monaghan@users.noreply.github.com>
2022-12-01 17:34:23 -0500

    test

--- js/script.js
+++ js/script.js
@@ -14,27 +14,37 @@ if (navigator.serviceWorker) {
     scope: "/ICS2OR-Assignment-4/",
   })
 }
+// Copyright (c) 2022 Ashley Monaghan All rights reserved
+//
+// Created by: Ashley Monaghan
+// Created on: Oct 2022
+// This file contains the JS functions for index.html
+
+"use strict"
 
 /**
- * This function displays an alert.
- * Math
+ * Check servie worker.
  */
-function calculate() {
-  // input
-  const a = parseInt(document.getElementById("textbox-a").value)
-  const b = parseInt(document.getElementById("textbox-b").value)
-  const c = parseInt(document.getElementById("textbox-c").value)
-
-  // process
-  const math = a + b + c
-  // output
-  document.getElementById("math").innerHTML = "Numbers: " + math + " units"
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2OR-Assignment-4/sw.js", {
+    scope: "/ICS2OR-Assignment-4/",
+  })
 }
 
 /**
  * This function displays an alert.
  * Words
  */
-function button() {
-  document.getElementById("words").innerHTML = "<p>Hello, World!</p>"
+function calculate() {
+  let userSize = document.getElementById("size").value
+  let userTopings = document.getElementById("topings").value
+
+  if (userSize == "small") {
+    let sizePrice = 1.50
+    document.getElementById("answer").innerHTML =
+    "It will be" + sizePrice
+  } else {
+    document.getElementById("answer").innerHTML =
+      "Error"
+  }
 }
