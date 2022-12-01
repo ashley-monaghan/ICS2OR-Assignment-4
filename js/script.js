1b2f270e61c86f26a67bc330fd56f516f7e91aec
Ash! <113056162+ashley-monaghan@users.noreply.github.com>
2022-12-01 18:14:21 -0500

    .

--- js/script.js
+++ js/script.js
@@ -41,27 +41,27 @@ function calculate() {
 
   let totalPrice = sizePrice + topingPrice
 
-  if (userSize == "Small") {
-    let sizePrice = 1.50
-  } else if (userSize == "Medium") {
-    let sizePrice = 2.00
-  } else if (userSize == "Large") {
-    let sizePrice = 2.50
-  } else if (userSize == "Extra-Large") {
-    let sizePrice = 2.50
-  } else {
-    document.getElementById("pizzaSize").innerHTML = "Error"
-  }
+    if (userSize == "Small") {
+      let sizePrice = 1.50
+    } else if (userSize == "Medium") {
+      let sizePrice = 2.00
+    } else if (userSize == "Large") {
+      let sizePrice = 2.50
+    } else if (userSize == "Extra-Large") {
+      let sizePrice = 2.50
+    } else {
+      document.getElementById("pizzaSize").innerHTML = "Error"
+    }
 
-  if (userTopings == "Extra-Cheese") {
-    let topingPrice = 50
-  } else if (userTopings == "Pepperoni") {
-    let topingPrice = 1
-  } else if (userTopings == "Pineapple") {
-    let topingPrice = 1.50
-  } else {
-    document.getElementById("pizzaSize").innerHTML = "Error"
-  }
+    if (userTopings == "Extra-Cheese") {
+      let topingPrice = 50
+    } else if (userTopings == "Pepperoni") {
+      let topingPrice = 1
+    } else if (userTopings == "Pineapple") {
+      let topingPrice = 1.50
+    } else {
+      document.getElementById("pizzaSize").innerHTML = "Error"
+    }
 
   function calculate() 
   document.getElementById("pizzaSize").innerHTML =
