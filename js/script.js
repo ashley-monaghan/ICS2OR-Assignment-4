947b7e151c7234538d415b3f220cf318d4f1ce3d
ashley-monaghan <ashley-monaghan@users.noreply.github.com>
2022-12-02 00:15:07 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -77,6 +77,9 @@ function calculate() {
 }
 
 function whenButtonClicked() {
-  document.getElementById("total").innerHTML = "Your total will be $" + sizePrice + topingPrice + "! Please call 737-1111 to order!"
-
+  document.getElementById("total").innerHTML =
+    "Your total will be $" +
+    sizePrice +
+    topingPrice +
+    "! Please call 737-1111 to order!"
 }
