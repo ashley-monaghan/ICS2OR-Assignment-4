95f2d1b0c2109847cfcc6a4d6b359a3ec9513c6c
Ash! <113056162+ashley-monaghan@users.noreply.github.com>
2022-12-01 20:41:18 -0500

    ...

--- js/script.js
+++ js/script.js
@@ -75,11 +75,8 @@ function calculate() {
     document.getElementById("pizzaTopings").innerHTML = "Error"
   }
 
-  function whenButtonClicked() {
-    document.getElementById("total").innerHTML =
-      "Your total will be $" +
-      sizePrice +
-      topingPrice +
-      "! Please call 737-1111 to order!"
-  }
+  function calculatee() {
+    document.getElementById("total").innerHTML = "Your total will be $" + sizePrice + topingPrice + "! Please call 737-1111 to order!"
+  }  
+
 }
