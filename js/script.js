6654a379f3e8bc987ec9a1ea1ff3d47ac1da97c2
ashley-monaghan <ashley-monaghan@users.noreply.github.com>
2022-12-14 18:12:17 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -39,27 +39,27 @@ function calculate() {
   let userSize = document.getElementById("size").value
   let userTopings = document.getElementById("topings").value
 
-  let sizePrice = 0.00
-  let topingPrice = 0.00
-  
+  let sizePrice = 0.0
+  let topingPrice = 0.0
+
   if (userSize == "Small") {
-    sizePrice = 1.00
+    sizePrice = 1.0
   } else if (userSize == "Medium") {
-    sizePrice = 1.50
+    sizePrice = 1.5
   } else if (userSize == "Large") {
-    sizePrice = 2.00
+    sizePrice = 2.0
   } else if (userSize == "Extra-Large") {
-    sizePrice = 2.50
+    sizePrice = 2.5
   } else {
     document.getElementById("pizzaSize").innerHTML = "Error"
   }
 
   if (userTopings == "Extra-Cheese") {
-    topingPrice = 0.50
+    topingPrice = 0.5
   } else if (userTopings == "Pepperoni") {
-    topingPrice = 1.00
+    topingPrice = 1.0
   } else if (userTopings == "Pineapple") {
-    topingPrice = 1.50
+    topingPrice = 1.5
   } else {
     document.getElementById("pizzaTopings").innerHTML = "Error"
   }
@@ -67,6 +67,6 @@ function calculate() {
   let totalPrice = sizePrice + topingPrice
 
   document.getElementById("total").innerHTML =
-    "Your total will be $" + totalPrice.toFixed(2); + "! Please call 737-1111 to order!"
-
+    "Your total will be $" + totalPrice.toFixed(2)
+  ;+"! Please call 737-1111 to order!"
 }
